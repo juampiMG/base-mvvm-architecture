4ea8b31aa03e08727afa55ea12d39490d409399c
Juampi <juanpablo.munoz@berepublic.es>
2018-08-28 23:12:48 +0200

    refactor loading behaviour

--- app/src/main/java/com/jp/app/common/viewModel/BaseViewModel.java
+++ app/src/main/java/com/jp/app/common/viewModel/BaseViewModel.java
@@ -3,6 +3,9 @@ package com.jp.app.common.viewModel;
 import android.arch.lifecycle.ViewModel;
 import android.content.Context;
 
+import com.jp.app.common.controller.BaseActivity;
+import com.jp.app.common.view.IBaseView;
+
 import java.lang.ref.WeakReference;
 
 import javax.inject.Inject;
@@ -10,7 +13,7 @@ import javax.inject.Inject;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 
-public abstract class BaseViewModel<N> extends ViewModel {
+public abstract class BaseViewModel<N extends IBaseView> extends ViewModel {
 
     @Inject
     Context mContext;
@@ -49,6 +52,24 @@ public abstract class BaseViewModel<N> extends ViewModel {
     public void addDisposable(Disposable disposable) {
         if (disposable != null && mCompositeDisposable != null) {
             mCompositeDisposable.add(disposable);
+        }
+    }
+
+    protected void hideLoading () {
+        if (mWeakReference.get() != null) {
+            mWeakReference.get().hideLoading();
+        }
+    }
+
+    protected void showLoading () {
+        if (mWeakReference.get() != null) {
+            mWeakReference.get().showLoading();
+        }
+    }
+
+    protected void showError (String title, String message, BaseActivity.actionOnError actionOnError) {
+        if (mWeakReference.get() != null) {
+            mWeakReference.get().showError(title, message, actionOnError);
         }
     }
 
